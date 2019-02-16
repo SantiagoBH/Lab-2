d107856957c95c1778f9cb4e79cadf6378deb8de
santiagoBH <santigobh20@gmail.com>
2019-02-16 16:49:24 -0500

    Teclado

---
+++ Teclado.ino
@@ -0,0 +1,41 @@
+#include <Servo.h>
+
+Servo servo1, servo2;
+
+char orden;
+
+void setup() 
+{
+  servo1.attach(9);
+  servo2.attach(13);
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+orden = Serial.read();
+
+switch (orden){
+  case 'w':
+  servo1.write(0);
+  servo2.write(0);
+  break;
+
+  case 's':
+  servo1.write(90);
+  servo2.write(90);  
+  break;
+
+  case 'a':
+  servo1.write(90);
+  servo2.write(85);  
+  break;
+
+  case 'd':
+  servo1.write(95);
+  servo2.write(90);  
+  break;
+
+  
+}
+}
